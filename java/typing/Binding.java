4434a7590b167dc8514547ada79845b362fe6472
Silveira Neto <silveiraneto@gmail.com>
2012-07-15 21:59:04 -0400

    full example with dynamic and static binding in java

--- java/typing/Binding.java
+++ java/typing/Binding.java
@@ -1,23 +1,37 @@
 class A {
 	public String foo(){
-		return "A";
+		return "A.foo";
+	}
+
+	public static String bar(){
+		return "A.bar";
 	}
 }
 
 class B extends A{
 	public String foo(){
-		return "B";
+		return "B.foo";
+	}
+	
+	public static String bar(){
+		return "B.bar";
 	}
 }
 
-public class Typing {
+public class Binding {
 	public static void main(String args[]) {
 		A a = new A();
 		B b = new B();
 		A c = new B(); 
 
-		System.out.println(a.foo()); // A
-		System.out.println(b.foo()); // B
-		System.out.println(c.foo()); // B
+		System.out.println("Dynamic Binding / Late Binding");
+		System.out.println(a.foo()); // A.foo
+		System.out.println(b.foo()); // B.foo
+		System.out.println(c.foo()); // B.foo
+
+		System.out.println("\nStatic Binding");
+		System.out.println(a.bar()); // A.bar
+		System.out.println(b.bar()); // B.bar
+		System.out.println(c.bar()); // A.bar
 	}
 }
