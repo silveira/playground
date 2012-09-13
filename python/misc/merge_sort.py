d406199c56ef596324e375339ca2fe08965244c6
Silveira Neto <silveiraneto@gmail.com>
2012-09-13 18:48:37 -0400

    recursive mergesort

---
+++ python/misc/merge_sort.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+def merge(left, right):
+	"""
+	>>> merge([2,3,7], [1,4,5])
+	[1,2,3,4,5,7]
+	"""
+	result = []
+	i ,j = 0, 0
+	while i < len(left) and j < len(right):
+		if left[i] <= right[j]:
+			result.append(left[i])
+			i += 1
+		else:
+			result.append(right[j])
+			j += 1
+	result += left[i:]
+	result += right[j:]
+	return result
+
+def mergesort(array):
+	if len(array)==0 or len(array)==1:
+		return array
+	elif len(array)==2:
+		if(array[0]>array[1]):
+			return [array[1], array[0]]
+		else:
+			return array
+	else:
+		mid = len(array)/2
+		left = mergesort(array[:mid])
+		right = mergesort(array[mid:])
+		return merge(left, right)
+
+if __name__=="__main__":
+#	print merge([2,3,7], [1,4,5])
+	print mergesort([3,1,4,1,5,9,2,6,5])
