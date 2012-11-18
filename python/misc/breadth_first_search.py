0dfe6374932ea3f3677e806df1e272c6790463d4
Silveira Neto <silveiraneto@gmail.com>
2012-11-18 12:31:13 -0500

    breadth first search algorithmn

---
+++ python/misc/breadth_first_search.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+
+"""
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'A', 'C'],
+    'C': ['A', 'D', 'E', 'B'],
+    'D': ['B', 'C', 'E'],
+    'E': ['C', 'D']
+}
+"""
+
+graph = {
+    'A': ['B', 'C', 'F'],
+    'B': ['C', 'A', 'D', 'E'],
+    'C': ['A', 'E', 'B', 'D'],
+    'D': ['B', 'E', 'I'],
+    'E': ['B', 'C', 'D'],
+    'I': ['D'],
+    'F': ['A', 'G', 'H'],
+    'G': ['H', 'F'],
+    'H': ['G', 'F']
+}
+
+def bfs(graph, root):
+    queue = [root]
+    visited = {root:True}
+    while(len(queue)!=0):
+        node = queue.pop(0)
+        print node
+        for neighbor in graph[node]:
+            if(not visited.has_key(neighbor)):
+                queue.append(neighbor)
+                visited[neighbor] = True
+
+bfs(graph, 'A')
