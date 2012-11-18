a0dfc9550bedeb2d1f4aab81cb7a806ca7217a54
Silveira Neto <silveiraneto@gmail.com>
2012-11-18 12:30:36 -0500

    preorder, inorder, and postorder t algorithms

---
+++ python/misc/binary_tree_traversal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+LEFT = 0
+RIGHT = 1
+
+example_graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['', ''],
+    'D': ['G', ''],
+    'E': ['', 'F'],
+    'F': ['', ''],
+    'G': ['', 'H'],
+    'H': ['I', ''],
+    'I': ['K', ''],
+    'K': ['', '']
+}
+
+def preorder(graph, root):
+    if (root==''):
+        return []
+    else:
+        return [root] + preorder(graph, graph[root][LEFT]) + preorder(graph, graph[root][RIGHT])
+
+def inorder(graph, root):
+    if (root==''):
+        return []
+    else:
+        return inorder(graph, graph[root][LEFT]) + [root] + inorder(graph, graph[root][RIGHT])
+
+def postorder(graph, root):
+    if (root==''):
+        return []
+    else:
+        return postorder(graph, graph[root][LEFT]) + postorder(graph, graph[root][RIGHT]) + [root]
+
+print preorder(example_graph, 'A')
+print inorder(example_graph, 'A')
+print postorder(example_graph, 'A')
