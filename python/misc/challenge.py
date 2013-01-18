877935098130f312b54e9548fd524e336898ec30
Silveira Neto <silveiraneto@gmail.com>
2013-01-18 18:26:05 -0500

    Fixing the use of key and putting some comments

--- python/misc/challenge.py
+++ python/misc/challenge.py
@@ -2,10 +2,17 @@
 
 # break a caesar cypher
 
-c = "QEFP FP QEB CFOPQ QBUQ"
-for k in xrange(27):
+# ciphered message
+c = "A QEFP FP QEB CFOPQ QBUQ XXXXX    "
+
+# count how many letters we have in the alphabet
+n = ord('Z') - ord('A')
+
+for k in xrange(n+1):
     print k,
     for letter in c:
-      if letter != '':
-        print chr(ord(letter)+k % ord('A')),
+      if letter != ' ':
+        print chr(ord('A') + ((ord(letter)+k)%n)),
+      else:
+        print letter,
     print 
